fecf1418db410cec3d2ca1f3f859a85a19ea0fd4
Belfast <interview@signifyd.com>
2019-11-27 08:45:22 +0000

    adding instructions

--- src/main/java/com/signifyd/Main.java
+++ src/main/java/com/signifyd/Main.java
@@ -1,5 +1,23 @@
 package com.signifyd;
 
+/*# Data has been exported from our legacy system.
+# We now require this to be moved to S3 to be loaded into our Redshift data warehouse
+# Write a script that will take the exported csv file provided and load it into S3
+
+# Considerations
+# The data in the file contains some incompatibilities with our redshift database you will need to fix these
+#
+# These are as follows:
+# The date format is incompatible this should be changed to yyyy-MM-dd format.
+# The amount column contains a special character which will need to be removed.
+# Your code should reflect any considerations you see fit in terms of best practices and working with datasets at scale.
+
+# Tips:
+# Do not over engineer your solution! Be mindful of the time you have allocated, your task is to get the file into S3 in
+# the correct format.
+
+# I have provided 2 functions in the file S3_Util with the correct bucket to load the files into, you should be able to
+# tweak one of these to your requirements to save you some time.*/
 
 public class Main {
 
